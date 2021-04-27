ab403d194849168cf579c22a24d6e196b62461eb
sganhy <sganhy@hotmail.com>
2021-04-28 00:49:39 +0200

    unitesting schema #1

--- schema/log_test.go
+++ schema/log_test.go
@@ -12,9 +12,30 @@ func Test__log__setters(t *testing.T) {
 		t.Errorf("log.setMethod() ==> method <> '555'")
 	}
 
-	// test left truncate max 80 char
+	// test right truncate max 80 char
 	logger.setMethod("The hour of the reference time is 15, or 3PM. The layout can express it either way, ...")
-	if logger.method != "The hour of the reference time is 15, or 3PM. The layout can express it either w" {
-		t.Errorf("log.setMethod() ==> method is not equal to 'The hour of the reference time is 15, or 3PM. The layout can express it either w'")
+	expectedText := "The hour of the reference time is 15, or 3PM. The layout can express it either w"
+	if logger.method != expectedText {
+		t.Errorf("log.setMethod() ==> method is not equal to '%s'", expectedText)
 	}
+
+	logger.setMessage("555")
+	if logger.message != "555" {
+		t.Errorf("log.setMessage() ==> message <> '555'")
+	}
+	longtext := "I have been fascinated with how the mind structures information for as long as I can remember. As a kid, my all-time favorite activity in middle school was diagramming sentences with their parts of speech. Perhaps it’s not surprising, then, that I ended up at MIT earning my doctorate on formal models of language and cognition."
+	// set right truncate max 255
+	logger.setMessage(longtext)
+	if len(logger.message) > 255 {
+		t.Errorf("log.setMessage() ==> len(logger.message) is greater than 255")
+	}
+	logger.setCallSite("555")
+	if logger.callSite != "555" {
+		t.Errorf("log.setCallSite() ==> callSite <> '555'")
+	}
+	logger.setCallSite(longtext)
+	if len(logger.callSite) > 255 {
+		t.Errorf("log.setCallSite() ==> len(logger.callSite) is greater than 255")
+	}
+
 }
--- schema/schema_test.go
+++ schema/schema_test.go
@@ -2,6 +2,8 @@ package schema
 
 import (
 	"ring/schema/databaseprovider"
+	"ring/schema/ddlstatement"
+	"ring/schema/entitytype"
 	"ring/schema/fieldtype"
 	"ring/schema/physicaltype"
 	"ring/schema/tabletype"
@@ -35,7 +37,15 @@ func Test__Schema__Init(t *testing.T) {
 	if schema.IsBaseline() != true {
 		t.Errorf("Schema.Init() ==> IsBaseline() <> true")
 	}
-
+	if schema.GetPhysicalName() != "test physical name" {
+		t.Errorf("Schema.Init() ==> physicalName() <> 'test physical name'")
+	}
+	if schema.GetDatabaseProvider() != databaseprovider.Influx {
+		t.Errorf("Schema.Init() ==> provider() <> databaseprovider.Influx")
+	}
+	if schema.GetEntityType() != entitytype.Schema {
+		t.Errorf("Schema.Init() ==> GetEntityType() <> entitytype.Schema")
+	}
 }
 
 // test GetTableByName, GetTableById
@@ -111,5 +121,32 @@ func Test__Schema__GetTableByName(t *testing.T) {
 	val = schema.GetTableByName("77777777")
 	if val != nil {
 		t.Errorf("Schema.GetTableById() ==> table name '77777777' cannot be found")
+	}
+}
+
+func Test__Schema__GetSequenceByName(t *testing.T) {
+	var schema = new(Schema)
+	schema = schema.getMetaSchema(databaseprovider.MySql, "", 0, 0, true)
+
+	for i := 0; i < len(schema.sequences); i++ {
+		sequence := schema.GetSequenceByName(schema.sequences[i].name)
+		if sequence == nil {
+			t.Errorf("Schema.GetSequenceByName() ==> sequence name '%s' cannot be found", schema.sequences[i].name)
+		}
+	}
+
+	sequence := schema.GetSequenceByName("????????????")
+	if sequence != nil {
+		t.Errorf("Schema.GetSequenceByName() ==> sequence name ????????????' must be null")
+	}
+
+}
+
+func Test__Schema__GetDdl(t *testing.T) {
+	var schema = new(Schema)
+	schema = schema.getMetaSchema(databaseprovider.MySql, "rpg_test", 0, 0, true)
+
+	if schema.GetDdl(ddlstatement.Create) != "CREATE SCHEMA rpg_sheet_test" {
+		t.Errorf("Schema.GetDdl() ==> is not equal to 'CREATE SCHEMA rpg_sheet_test'")
 	}
 }
